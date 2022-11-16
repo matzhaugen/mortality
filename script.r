824f78659e20bb4afefbe6c39336ec3e8ea1903f
Matz Haugen <mhaugen@vise.com>
2022-11-16 17:01:54 +0100

    fix bugs in all countries script

--- script.r
+++ script.r
@@ -1,1 +1,3 @@
 install.packages(c("stringr", "dplyr", "ggplot2", "lubridate", "ISOweek", "scales", "zoo", "tidyr", "quantreg", "splines", "pbs", "forecast", "fpp2", "xts", "RcppRoll"))
+install.packages('IRkernel')
+IRkernel::installspec()  # to register the kernel in the current R installation
